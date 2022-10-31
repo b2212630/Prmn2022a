a0f8f715297cca4eefcd3f2a9ad8cb76474ea677
b2212630 <b2212630@photon.chitose.ac.jp>
2022-10-31 18:06:34 +0900

    課題1-3

--- Prmn2022a/src/lecture01/Main.java
+++ Prmn2022a/src/lecture01/Main.java
@@ -2,6 +2,10 @@ package lecture01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("B" + 212630 + " Hina Yoshida");
+        int age = 23;
+        if(age < 20){
+            System.out.println("I am "+age + "years old so I cannot drink liquor.");
+        }
+        System.out.println("I am "+age+ "years old so I can drink liquor.");
     }
 }
