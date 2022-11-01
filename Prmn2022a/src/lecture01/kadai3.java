25c240cdb1dddedb6815ba9a0d0e16dd6ee281ce
b2212630 <b2212630@photon.chitose.ac.jp>
2022-11-01 22:42:35 +0900

    課題1-3

--- Prmn2022a/src/lecture01/kadai3.java
+++ Prmn2022a/src/lecture01/kadai3.java
@@ -1,4 +1,12 @@
 package lecture01;
 
 public class kadai3 {
+    public static void main(String[] args) {
+        System.out.println("B" + 212630 + " Hina Yoshida");
+        int age = 23;
+        if(age < 20){
+            System.out.println("I am "+age + "years old so I cannot drink liquor.");
+        }
+        System.out.println("I am "+age+ "years old so I can drink liquor.");
+    }
 }
