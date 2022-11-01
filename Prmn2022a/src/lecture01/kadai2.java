320286d776371bbb279681eaa61c8ef0349247c0
b2212630 <b2212630@photon.chitose.ac.jp>
2022-11-01 22:41:13 +0900

    課題1-2

---
+++ Prmn2022a/src/lecture01/kadai2.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class kadai2 {
+    public static void main(String[] args) {
+        System.out.println("B212630 Hina Yoshida");
+        System.out.println("B" + 212630 + " Hina Yoshida");
+    }
+}
